5e941b7d3cda3642a16ab29d44c53bd1fae66e08
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-04 13:03:56 -0500

    Interim checkin

--- lib/student.rb
+++ lib/student.rb
@@ -5,12 +5,17 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.keys.each |key|
-      [key] == student_hash[key]
+    student_hash.keys.each do |key|
+      self.send("#{key}=", student_hash[key])
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    student_array.each do |first, last|
+      
+    end  
+    @@all << student_array
   end
 
   def add_student_attributes(attributes_hash)
